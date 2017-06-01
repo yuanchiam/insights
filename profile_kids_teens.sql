70278d82c4f7615cbb7080ad5cd61f39f58d6a02
yuanchiam <ychiam@netflix.com>
2017-06-01 09:31:26 -0700

    Update profile_kids_teens.sql

--- profile_kids_teens.sql
+++ profile_kids_teens.sql
@@ -1,27 +1,23 @@
+
 select 
+a.account_id,
+sum(a.profile_group_tmp) as profile_group
+from
+(select 
 account_id,
-from_unixtime(CAST(create_ts/1000 as BIGINT), 'yyyy-MM-dd') as create_time,
-experience_type,
-maturity_level_desc,
+--from_unixtime(CAST(create_ts/1000 as BIGINT), 'yyyyMMdd') as create_time,
 case when experience_type in ('Unknown','regular') then 10000
      when experience_type='just_for_kids' and maturity_level_desc='ADULTS' then 1000
      when experience_type='just_for_kids' and maturity_level_desc='TEENS' then 100
      when experience_type='just_for_kids' and maturity_level_desc='OLDER_KIDS' then 10
      when experience_type='just_for_kids' and maturity_level_desc='LITTLE_KIDS' then 1
-     else 0 end as profile_group
---82,080,675
---select count(*) -- member_since_ts<1488326399000
---129,638,628
---select count(*)
-select count(*), experience_type,
-maturity_level_desc
+     else 0 end as profile_group_tmp
 from dse.profile_d
 where profile_type='streaming'
 and profile_first_use_flag is not null
 and membership_status_id=2
 -- feb 28, 2017
-and member_since_ts<1488326399000
+and create_ts<1488326399000
 and is_deleted_by_user=0
-and is_deleted_from_source is null
-group by experience_type,
-maturity_level_desc
+and is_deleted_from_source is null) a
+group by account_id
